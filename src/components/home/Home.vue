9380fdafc0e565ba1bb3804abf61431c34f223b1
Yang Haitao <583364851@qq.com>
2020-01-13 16:55:10 +0800

    修改样式

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <section class="home">
-    <header>
-      <a>
-        <img alt="" src="@assets/logo.png" class="home_img"/>
+    <header class="home_head">
+      <a @click="$router.push('/home')">
+        <img alt="" src="@assets/logo.png" class="home_img" />
       </a>
+      <el-input v-model="message" style="width: 200px;" placeholder="请输入内容"></el-input>
+      <section>
+        <span>登录</span> |
+        <span>注册</span>
+      </section>
     </header>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      message: ""
+    };
+  }
+};
 </script>
 
 
@@ -17,8 +28,13 @@ export default {};
 .home {
   height: 5rem;
   .home_img {
-      height: 50px;
-      cursor: pointer;
+    height: 50px;
+    cursor: pointer;
+  }
+  .home_head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
